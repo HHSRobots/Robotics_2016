347493a3fbca505ee663b4d1d07825ab039244b5
Robot <tonyjkremer@gmail.com>
2016-01-30 11:43:22 -0500

    Beater bar edit 1
    added talon support in the subsystem

--- src/org/usfirst/frc/team554/robot/subsystems/BeaterBar.java
+++ src/org/usfirst/frc/team554/robot/subsystems/BeaterBar.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team554.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 /**
  *
@@ -9,10 +12,11 @@ public class BeaterBar extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	private SpeedController BeaterBarMotor;
 	
 	public BeaterBar(){
 		super();
+		BeaterBarMotor = new Talon(3);//Talon number up for debate.
 		
 	}
 	
