14065b31dd56914173c82cd82497faff8427c412
Robot <tonyjkremer@gmail.com>
2016-02-02 20:06:44 -0500

    drive train stuff
    programmed it so that the joystidck has two modes based upon the
    trigger.

--- src/org/usfirst/frc/team554/robot/commands/DriveTrain_JoyStickDrive.java
+++ src/org/usfirst/frc/team554/robot/commands/DriveTrain_JoyStickDrive.java
@@ -20,7 +20,7 @@ public class DriveTrain_JoyStickDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.oi.getOperator(); // aww
+    	Robot.drivetrain.drive(Robot.oi.getOperator()); // aww
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -30,7 +30,7 @@ public class DriveTrain_JoyStickDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.drivetrain.drivemanual(0,0);
+    	Robot.drivetrain.drivemanual(0.0,0.0);
     }
 
     // Called when another command which requires one or more of the same
