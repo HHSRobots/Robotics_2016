8a74fcc60e4941bb4e6b7793a8ceace154e73cd9
Robot <tonyjkremer@gmail.com>
2016-02-06 10:33:31 -0500

    Xbox One configuratino oier'[g'pbg

--- src/org/usfirst/frc/team554/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team554/robot/subsystems/DriveTrain.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.VictorSP;
 
 import org.usfirst.frc.team554.robot.commands.DriveTrain_JoyStickDrive;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -21,11 +22,12 @@ public class DriveTrain extends Subsystem {
     private Encoder left_encoder, right_encoder;
     private AnalogGyro gyro;
     private Solenoid gearShiftSolenoid;
+    private boolean wasHeld;
     
     public DriveTrain(){
     	super();
-    	left_wheels = new Talon(9);
-    	right_wheels = new Talon(8);
+    	left_wheels = new VictorSP(9);
+    	right_wheels = new VictorSP(8);
     	drive = new RobotDrive(left_wheels, right_wheels);
     	left_encoder = new Encoder(2,3);
     	right_encoder = new Encoder(4,5);
@@ -35,6 +37,7 @@ public class DriveTrain extends Subsystem {
     	right_encoder.setDistancePerPulse(0.01745);
     	
     	gyro = new AnalogGyro(0);
+    	wasHeld=false;
     }
     
     // Put methods for controlling this subsystem
@@ -49,13 +52,24 @@ public class DriveTrain extends Subsystem {
     }
     
     public void drive(Joystick joystick_driver){
-    	double turn = -joystick_driver.getZ()*3/4;
-    	if (!joystick_driver.getRawButton(1))
-    	{
-    		turn=gyro.getRate();//instead of turn being dictated by the person, the turn should be the opposite how it's turning.
-    		//make sure that this actually ensures that it will turn the opposite way it's moving. It may spin uncontrollably without proper set up.
+    	double z; // amount of rotation applied to robot -128 through 128, 0 is none
+    	double Kp = 0.025; // constant that gives magnitude of rotation correction (recomended is 0.03)
+    	
+    	if (joystick_driver.getRawButton(12) == true ){	
+    		z =-joystick_driver.getZ();
+    		wasHeld = true;
+    	} else if (wasHeld && Math.abs(gyro.getRate()) <= 1 ){
+    		gyro.reset();
+    		wasHeld = false;
+    		double angle = gyro.getAngle();
+    		z = Kp * angle;
+    	} else {
+    	// inset gyro balancing code here to compensate for skew
+    		double angle = gyro.getAngle();
+    		z = Kp * angle;
     	}
-    	drive.arcadeDrive(-joystick_driver.getY(),turn);
+    	
+    	drive.arcadeDrive(joystick_driver.getY(),z);
     }
     
     
